b7a138b7b280460816cf9cc1fb264605a0b6ccf0
Michael Rodgers <38140530+michaelrodgers@users.noreply.github.com>
2019-01-08 17:15:12 -0800

    Adds error message if unit not selected

--- tempconversion.php
+++ tempconversion.php
@@ -4,9 +4,9 @@
  <body> 
  <h2>Temperature Conversion</h2> 
  <form action = "<?php echo $_SERVER['PHP_SELF']; ?>" method = "GET"> 
- Degrees: 
+ Degrees:
  <input type = "text" name = "degree" size=10> <br />
-     
+ Please select one:<br />
  <input type="radio" value="celcius" name="scale"> Celcius <br />
  <input type="radio" value="fahrenheit" name="scale"> Fahrenheit <br />
  <input type="radio" value="kelvin" name="scale"> Kelvin <br />
@@ -21,11 +21,10 @@
 	$degree = $_GET['degree'];
 	$firstLength = strlen($_GET['degree']);
 	
-	//check that input is not NULL or NaN
 	if(($firstLength > 0) && (is_numeric($_GET['degree']))){
 		if ($scale == "celcius") 
 		{
-			 print "<table><tr><th> Conversion Results</th></tr><tr><td>$degree</td><td>Celsius</td></tr>"; 
+			 print "<table border><tr><th> Conversion Results</th></tr><tr><td>$degree</td><td>Celsius</td></tr>"; 
 			 $c_2_f = round($degree*9/5+32, 2); 
 			 print "<tr><td>$c_2_f</td><td>Fahrenheit</td></tr>"; 
 			 $c_2_k = round($degree+273.15, 2); 
@@ -33,7 +32,7 @@
         } 
 		if ($scale == "fahrenheit") 
 		{
-			 print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td>$degree</td><td>Farhenheit</td></tr>"; 
+			 print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td>$degree</td><td>Fahrenheit</td></tr>"; 
 			 $f_2_c = round(($degree -32)*5/9, 2); 
 			 print "<tr><td>$f_2_c</td><td>Celsius</td></tr>"; 
 			 $f_2_k = round($f_2_c+273.15, 2); 
@@ -48,9 +47,16 @@
 			 $k_2_c = round($degree-273.15, 2); 
 			 print "<tr><td>$k_2_c </td><td>Celsius</td></tr>"; 
 		} 
+        //check that input is not NULL
+        if (is_null($scale))
+        {
+        //print an error message if scale is not selected
+		echo "<span style = \"color:red\">*Please Select A Unit of Measure.</span>";
+        }
      } else {
 		//print an error message if input is NULL or NaN
 		echo "<span style = \"color:red\">*Please Enter A Valid Temperature.</span>";
      }
 }
 ?> 
+</body>
